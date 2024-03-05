7451f9292c7dfd05c171becd77651f4efa467ab0
Shivam Panwar <119106604+shivampanwar15@users.noreply.github.com>
2024-03-06 04:01:44 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const MongoString = process.env.MONGODB_URI;
 console.log(MongoString);
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://shivampanwar:asdfghjkl@clustervoosh.j8yy2b0.mongodb.net/?retryWrites=true&w=majority&appName=ClusterVoosh', {
+        await mongoose.connect(MongoString, {
             useNewUrlParser: true,
         });
         console.log('MongoDB Connected');
